2f62e582cb0a335e8741c045ba9ca0f9a56aac43
SunShine <07033320a@gmail.com>
2016-11-03 17:50:44 +0800

    init

---
+++ src/main/java/ru/kosinov/NettySocketioSpringApplication.java
@@ -0,0 +1,39 @@
+package ru.kosinov;
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIONamespace;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class NettySocketioSpringApplication {
+
+    @Value("${wss.server.host}")
+    private String host;
+
+    @Value("${wss.server.port}")
+    private Integer port;
+
+    @Bean
+    public SocketIOServer socketIOServer() {
+        Configuration config = new Configuration();
+        config.setHostname(host);
+        config.setPort(port);
+        SocketIOServer server = new SocketIOServer(config);
+        server.addNamespace("/chat1");
+        return server;
+    }
+
+    @Bean
+    public SpringAnnotationScanner springAnnotationScanner(SocketIOServer ssrv) {
+        return new SpringAnnotationScanner(ssrv);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettySocketioSpringApplication.class, args);
+    }
+}
